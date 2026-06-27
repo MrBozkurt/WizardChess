258d8d73525941c532624b71bb0aadecbafd3b7e
MrBozkurt <emrejhj4@gmail.com>
2026-06-28 02:50:29 +0300

    Working chess

--- main.py
+++ main.py
@@ -1,13 +1,158 @@
+import chess
 import stt
+import os
+
+
+pieces_map = {
+        "pawn": chess.PAWN, "knight": chess.KNIGHT, "bishop": chess.BISHOP,
+        "rook": chess.ROOK, "queen": chess.QUEEN, "king": chess.KING
+    }
+
+def parse_stt(parse, board):
+    move = None
+
+    #print(parse.piece_list)
+    #print(parse.source)
+    #print(parse.dest)
+
+    # Source to destination
+    if (parse.source and parse.dest):
+        move = chess.Move.from_uci(parse.source + parse.dest)
+
+    # Piece to destination
+    elif (len(parse.piece_list) == 1 and parse.dest):
+        piece = parse.piece_list[0]
+        pieces = board.pieces(pieces_map[piece], board.turn)
+
+        is_multiple = False
+
+        for square in pieces:
+            square_name = chess.square_name(square)
+            temp_move = chess.Move.from_uci(square_name + parse.dest)
+            if temp_move in board.legal_moves:
+                if (not is_multiple):
+                    move = temp_move
+                    is_multiple = True
+                else:
+                    print("Multiple possibilites!")
+                    return move
+
+    # Piece to piece
+    elif (len(parse.piece_list) == 2):
+        source = parse.piece_list[0]
+        sources = board.pieces(pieces_map[source], board.turn)
+
+        taken = parse.piece_list[1]
+        takens = board.pieces(pieces_map[taken], not board.turn)
+
+        # One square given
+        if (parse.dest):
+            location = chess.parse_square(parse.dest)
+
+            found_square = None
+            found_target = None
+
+            # Source square given
+            for square in sources:
+                if (square == location):
+                    target_squares = board.attacks(square)
+                    found_square = square
+
+                    for target_square in target_squares:
+                        target_piece = board.piece_at(target_square)
+                        if (target_piece):
+                            if (target_piece.piece_type == pieces_map[parse.piece_list[1]]):
+                                if (found_target):
+                                    print("Multiple possible targets!")
+                                    move = None
+                                    return move
+                                else:
+                                    found_target = target_square
+                                    move = chess.Move.from_uci(chess.square_name(found_square) + chess.square_name(found_target))
+
+            if (found_square or found_target):
+                return move
+            
+            # Target square given
+            for square in takens:
+                if (square == location):
+                    attacker_squares = board.attackers(board.turn, square)
+                    found_target = square
+
+                    print(".")
+
+                    for attacker_square in attacker_squares:
+                        attacker_piece = board.piece_at(attacker_square)
+                        if (attacker_piece):
+                            print("..")
+                            if (attacker_piece.piece_type == pieces_map[parse.piece_list[0]]):
+                                print("...")
+                                if (found_square):
+                                    print("Multiple possible attackers!")
+                                    move = None
+                                    return move
+                                else:
+                                    found_square = attacker_square
+                                    move = chess.Move.from_uci(chess.square_name(found_square) + chess.square_name(found_target))
+
+        # No square given
+        else:
+            for square in sources:
+                target_squares = board.attacks(square)
+                for target_square in target_squares:
+                    target_piece = board.piece_at(target_square)
+                    if (target_piece):
+                        if (target_piece.piece_type == pieces_map[parse.piece_list[1]]):
+                            if (move):
+                                print("Multiple possible targets!")
+                                move = None
+                                return move
+                            else:
+                                move = chess.Move.from_uci(chess.square_name(square) + chess.square_name(target_square))
+
+    print(move)
+    return move
+
+def piece_to_code(piece):
+    if (piece):
+        if (piece == "knight"):
+            piece = "n"
+        else:
+            piece = piece[0]
 
 def main():
     stt.setup()
+    os.system('cls' if os.name=='nt' else 'clear')
+    board = chess.Board()
+
+    print(board)
+    print("WHITE" if board.turn else "black")
+
     while True:
         text = input()
+        move = None
+
         if (text == "a"):
-            stt.parseSound()
-        else:
+            sound = stt.parseSound()
+            move = parse_stt(sound, board)
+        elif (text=="z" or text=="b"):
             break
+        elif (text == "reverse"):
+            board.pop()
+        else:
+            parse_text = stt.parse(text)
+            move = parse_stt(parse_text, board)
+
+        if (move):
+            if move in board.legal_moves:
+                board.push(move)
+            else:
+                print("Not a legal move!")
+        
+        os.system('cls' if os.name=='nt' else 'clear')
+        print(board)
+        print("WHITE" if board.turn else "black")
+                
 
 if __name__ == "__main__":
     main()
--- stt.py
+++ stt.py
@@ -4,18 +4,29 @@ import pyaudio
 import time
 from vosk import Model, KaldiRecognizer
 
+class Parse:
+    piece_list = []
+    source = None
+    dest = None
+    
+    def clear(self):
+        self.piece_list = []
+        self.source = None
+        self.dest = None
+
 def setup():
     # Chess vocabulary
-    global pieces, files, ranks, ranks_map, actions, filler, chess_vocabulary, grammar_json, recognizer, stream, WAIT_TIME
-    pieces = ["pawn", "knight", "bishop", "rook", "queen", "king", "castle"]
+    global pieces, files, ranks, ranks_map, inv_ranks_map, actions, filler, chess_vocabulary, grammar_json, recognizer, stream, WAIT_TIME
+    pieces = ["pawn", "knight", "bishop", "rook", "queen", "king"]
     files = ["a", "b", "c", "d", "e", "f", "g", "h"]
     ranks = ["one", "two", "three", "four", "five", "six", "seven", "eight"]
     ranks_map = {
         "one": "1", "two": "2", "three": "3", "four": "4", 
         "five": "5", "six": "6", "seven": "7", "eight": "8"
     }
+    inv_ranks_map = {v: k for k, v in ranks_map.items()}
     actions = ["takes", "promotes"]
-    filler = ["from", "my", "the", "goes"]
+    filler = ["from", "the", "goes"]
     chess_vocabulary = pieces + files + ranks + actions + filler + ["[unk]"]
     grammar_json = json.dumps(chess_vocabulary)
 
@@ -45,80 +56,83 @@ def setup():
 
 def parse(text):
     text = text.split()
+    parse = Parse()
+    parse.clear()
 
-    piece_list = []
     locations = []
     action_list = []
 
-    piece = "None"
-    source = "None"
-    dest = "None"
-    target = "None"
-    promote = False
 
-    # Remove fillers
-    for word in text:
+    # Remove fillers and turn numbers to letters
+    for i in range(len(text)):
+        word = text[i]
         if (word in filler):
             text.remove(word)
+        elif (word in inv_ranks_map):
+            text[i] = inv_ranks_map[word]
     
     # Parse the command
     for i in range(len(text)):
         word = text[i]
 
-        if (word == "two" and i > 0):
-            prev_word = text[i-1]
-            if (prev_word not in files):
-                action_list.append("to")
+        if (i < len(text) - 1 and word == "eight" and text[i+1] in ranks):
+            word = "a"
 
         elif (word in pieces):
-            piece_list.append(word)
+            parse.piece_list.append(word)
 
         elif (word in actions):
             action_list.append(word)
         
-        elif (word in files):
+        if (word in files):
             if (i > len(text) - 2 or text[i+1] not in ranks):
                 print("Command not in correct format!")
                 return False
             else:
                 locations.append(word + ranks_map[text[i+1]])
-        
-        else:
-            if (i == 0 or text[i-1] not in files):
-                print("Command not in correct format!")
-                return False
 
-    # Fill the empty variables
-    if (len(piece_list) < 1):
-        print("Command not in correct format!")
+        elif (word[0] in files and word[1].isnumeric() and len(word) == 2):
+            locations.append(word)
+
+        elif (word[0] in files and word[1].isnumeric() and word[2] in files and word[3].isnumeric() and len(word) == 4):
+            locations.append(word[0:2])
+            locations.append(word[2:4])
+        
+        # Number but no letter before
+        #else:
+        #    if (i == 0 or text[i-1] not in files):
+        #        print("Command not in correct format!")
+        #        return False
+
+
+    # No piece given, not enough squares given
+    if (len(parse.piece_list) < 1 and len(locations) <= 1):
+        print("No source or destination square!")
         return
     
-    piece = piece_list[0]
-
-    if ("promotes" in action_list):
-        promote = True
+    # Too much information
+    if (len(parse.piece_list) > 2 or len(locations) > 2):
+            print("Too many pieces or squares given!")
+            return
 
     # Piece takes another
     if (len(locations) == 0):
-        if (len(piece_list) != 2):
-            print("Command not in correct format!")
+        if (len(parse.piece_list) < 2):
+            print("No source or destination piece!")
             return
-        else:
-            target = piece_list[1]
+
     # Simple move
     elif (len(locations) == 1):
-        dest = locations[0]
+        parse.dest = locations[0]
     # Source given move
     elif (len(locations) == 2):
-        source = locations[0]
-        dest = locations[1]
-    else:
-        print("Command not in correct format!")
-        return
+        parse.source = locations[0]
+        parse.dest = locations[1]
     
-    print("Order: piece, source, destination, target, promotion")
-    print(f"{piece} {source} {dest} {target} {promote}\n")
-    return [piece, source, dest, target, promote]
+    #print("Order: pieces, source, destination")
+    #print(f"{parse.piece_list} {parse.source} {parse.dest}\n")
+
+    return parse
 
 
 def parseSound():
@@ -166,6 +180,7 @@ def parseSound():
         if running_text and (time.time() - last_speech_time > WAIT_TIME):
                 print(f"Validated Chess Command: {text}")
                 running_text = ""
-                if (parse(text)):
-                    break
+                parsed_text = parse(text)
+                if (parsed_text):
+                    return(parsed_text)
                 
